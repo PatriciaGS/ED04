50e92e730807a91c3b09bf66b2b52007ee055615
PatriciaGS <patri309@gmail.com>
2020-01-05 21:07:50 +0100

    javadoc

--- cuentas/cuentas/CCuenta.java
+++ cuentas/cuentas/CCuenta.java
@@ -1,16 +1,39 @@
 package cuentas;
+// TODO: Auto-generated Javadoc
+
+/**
+ * The Class CCuenta.
+ */
 public class CCuenta {
 
 
+    /** The nombre. */
     private String nombre;
+    
+    /** The cuenta. */
     private String cuenta;
+    
+    /** The saldo. */
     private double saldo;
+    
+    /** The tipo interés. */
     private double tipoInterés;
 
+    /**
+     * Instantiates a new c cuenta.
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Instantiates a new c cuenta.
+     *
+     * @param nom the nom
+     * @param cue the cue
+     * @param sal the sal
+     * @param tipo the tipo
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
@@ -18,11 +41,22 @@ public class CCuenta {
         setSaldo(sal);
     }
 
+    /**
+     * Estado.
+     *
+     * @return the double
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Ingresar.
+     *
+     * @param cantidad the cantidad
+     * @throws Exception the exception
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -30,6 +64,12 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Retirar.
+     *
+     * @param cantidad the cantidad
+     * @throws Exception the exception
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -39,34 +79,74 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+	/**
+	 * Gets the nombre.
+	 *
+	 * @return the nombre
+	 */
 	String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Sets the nombre.
+	 *
+	 * @param nombre the new nombre
+	 */
 	void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Gets the cuenta.
+	 *
+	 * @return the cuenta
+	 */
 	String getCuenta() {
 		return cuenta;
 	}
 
+	/**
+	 * Sets the cuenta.
+	 *
+	 * @param cuenta the new cuenta
+	 */
 	void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+	/**
+	 * Gets the saldo.
+	 *
+	 * @return the saldo
+	 */
 	double getSaldo() {
 		return saldo;
 	}
 
+	/**
+	 * Sets the saldo.
+	 *
+	 * @param saldo the new saldo
+	 */
 	void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * Gets the tipo interés.
+	 *
+	 * @return the tipo interés
+	 */
 	double getTipoInterés() {
 		return tipoInterés;
 	}
 
+	/**
+	 * Sets the tipo interés.
+	 *
+	 * @param tipoInterés the new tipo interés
+	 */
 	void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
